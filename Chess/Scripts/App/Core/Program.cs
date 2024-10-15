2e053996704497fd124affb8c2b45e87bcf402f4
AndriaBeridze <andria24b@gmail.com>
2024-10-15 10:55:03 -0400

    Set up .NET

---
+++ Chess/Scripts/App/Core/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ChessBot {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Chess Bot by Andria Beridze");
+        }
+    }
+}
